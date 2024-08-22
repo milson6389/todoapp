b53614ea9422de61bedfd3f83e14f85d62195d08
milson6389 <audie.milson38@gmail.com>
2024-08-23 02:25:14 +0700

    Feat: Implement Add Task Functionality & Load Table from Local Storage Data

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
+// Bagian Form Input
 const inputTodo = document.getElementById("taskTodo");
 const inputDueDate = document.getElementById("taskDueDate");
 const inputPriority = document.getElementById("taskPriority");
 const buttonSearch = document.getElementById("searchTask");
 const buttonAdd = document.getElementById("addTask");
 const todoForm = document.querySelector("#todoForm form");
+const buttonDeleteAll = document.getElementById("clearTodo");
 
 const titleEmpty = document.querySelector("p.titleEmpty");
 const priorityEmpty = document.querySelector("p.priorityEmpty");
 
-let todos = [];
+// Bagian Table
+const newTaskTable = document.getElementById("new");
+const newTaskTableBody = document.getElementById("newTaskRow");
+const inpTaskTable = document.getElementById("inprogress");
+const doneTaskTable = document.getElementById("complete");
+const lateTaskTable = document.getElementById("overdue");
 
+// Function to sort array of object by object's property
+const sortAscBy = (arr, prop, isDateType = false) => {
+  if (isDateType) {
+    return arr.sort((a, b) => new Date(a[prop]) - new Date(b[prop]));
+  }
+  return arr.sort((a, b) => a[prop] - b[prop]);
+};
+const sortDscBy = (arr, prop, isDateType = false) => {
+  if (isDateType) {
+    return arr.sort((a, b) => new Date(b[prop]) - new Date(a[prop]));
+  }
+  return arr.sort((a, b) => b[prop] - a[prop]);
+};
+
+// Function to format date with custom format "Day, date month year"
 const dateFormatter = (date) => {
   const daysOfWeek = [
     "Sunday",
@@ -44,6 +66,76 @@ const dateFormatter = (date) => {
   return result;
 };
 
+const Status = Object.freeze({
+  NEW: "new",
+  INP: "inprogress",
+  DONE: "done",
+  LATE: "late",
+});
+
+let todos = [];
+if (todos.length == 0 && localStorage.getItem("todo")) {
+  todos = [...JSON.parse(localStorage.getItem("todo"))];
+}
+let currentTodos = [];
+let new_todos = [];
+let inp_todos = [];
+let done_todos = [];
+let late_todos = [];
+
+// repopulate array after changes (add, edit, delete) & search queries
+const refreshArrayContentSource = (
+  source,
+  query = {
+    task: "",
+    startDt: new Date().toLocaleDateString(),
+    priority: [1, 2, 3],
+  }
+) => {
+  currentTodos = source.filter(
+    (todo) =>
+      todo.task.includes(query.task) &&
+      query.priority.includes(todo.priority) &&
+      todo.startDt == query.startDt
+  );
+  currentTodos = sortAscBy(currentTodos, "priority");
+  new_todos = currentTodos.filter((todo) => todo.status === Status.NEW);
+  inp_todos = currentTodos.filter(
+    (todo) => todo.status === Status.INP || todo.status === Status.DONE
+  ); //tujuannya agar task tetap ada di list inprogress tapi tampilannya tercoret
+  inp_todos = sortDscBy(inp_todos, "status");
+  done_todos = currentTodos.filter((todo) => todo.status === Status.DONE);
+  late_todos = source.filter(
+    (todo) =>
+      todo.status !== Status.DONE &&
+      todo.dueDt < new Date().toLocaleDateString()
+  );
+};
+
+// repopulate table layout -> show table if array length > 0
+const refreshTableLayout = () => {
+  newTaskTable.setAttribute("hidden", true);
+  inpTaskTable.setAttribute("hidden", true);
+  doneTaskTable.setAttribute("hidden", true);
+  lateTaskTable.setAttribute("hidden", true);
+
+  if (new_todos.length > 0) {
+    newTaskTable.removeAttribute("hidden");
+  }
+  if (inp_todos.length > 0) {
+    inpTaskTable.removeAttribute("hidden");
+  }
+  if (done_todos.length > 0) {
+    doneTaskTable.removeAttribute("hidden");
+  }
+  if (late_todos.length > 0) {
+    lateTaskTable.removeAttribute("hidden");
+  }
+};
+
+todos = sortDscBy(todos, "startDt", true);
+todos = sortAscBy(todos, "priority");
+
 const addTodo = () => {
   if (inputTodo.value.trim() !== "" && inputPriority.value.trim() !== "") {
     titleEmpty.setAttribute("hidden", true);
@@ -55,15 +147,94 @@ const addTodo = () => {
       dueDt: inputDueDate.value
         ? new Date(inputDueDate.value).toLocaleDateString()
         : new Date().toLocaleDateString(),
-      priority: inputPriority.value,
-      isTaken: false,
-      isDone: false,
+      priority: parseInt(inputPriority.value),
+      status: Status.NEW,
     };
     todos.push(newTodo);
     localStorage.setItem("todo", JSON.stringify(todos));
   } else {
     titleEmpty.removeAttribute("hidden");
     priorityEmpty.removeAttribute("hidden");
+  }
+  loadTableData();
+};
+
+const searchTodo = () => {
+  let searchByTitle = "";
+  let searchByDate = new Date().toLocaleDateString();
+  let priorities = [1, 2, 3];
+
+  if (inputTodo.value.trim() !== "") {
+    searchByTitle = inputTodo.value;
+  }
+  if (inputDueDate.value) {
+    searchByDate = new Date(inputDueDate.value).toLocaleDateString();
+  }
+  if (inputPriority.value.trim() !== "") {
+    priorities = [parseInt(inputPriority.value)];
+  }
+  let searchQueries = {
+    task: searchByTitle,
+    startDt: searchByDate,
+    priority: priorities,
+  };
+  refreshArrayContentSource(todos, searchQueries);
+};
+
+const generateTableRowData = (data) => {
+  let checkedStyleClass = data.status === Status.DONE ? "taskChecked" : "";
+  let taskPriorityStyleClass = "";
+  let taskPriorityText = "";
+  switch (data.priority) {
+    case 1:
+      taskPriorityText = "High";
+      taskPriorityStyleClass = "priorityHigh";
+      break;
+    case 2:
+      taskPriorityText = "Medium";
+      taskPriorityStyleClass = "priorityMed";
+      break;
+    default:
+      taskPriorityText = "Low";
+      taskPriorityStyleClass = "priorityLow";
+      break;
+  }
+
+  let template = `
+    <tr class="${checkedStyleClass}" id="${data.id}">
+      <td>
+        <input type="checkbox" name="takeTask" id="takeTask_${data.id}" />
+      </td>
+      <td>${data.task}</td>
+      <td class="${taskPriorityStyleClass}">${taskPriorityText}</td>
+      <td>${dateFormatter(data.startDt)}</td>
+      <td>${dateFormatter(data.dueDt)}</td>
+      <td>
+        <button class="deleteTodo">
+          <i class="fa-solid fa-trash"></i>
+        </button>
+      </td>
+    </tr>
+  `;
+  return template;
+};
+
+const loadTableData = () => {
+  refreshArrayContentSource(todos);
+  let allNewTaskList = "";
+  for (let i = 0; i < new_todos.length; i++) {
+    allNewTaskList += generateTableRowData(new_todos[i]);
+  }
+  newTaskTableBody.innerHTML = allNewTaskList;
+  refreshTableLayout();
+};
+
+const deleteAllTodo = () => {
+  if (confirm("Delete All Todo ?")) {
+    todos = [];
+    localStorage.removeItem("todo");
+    refreshArrayContentSource(todos);
+    refreshTableLayout();
   }
 };
 
@@ -72,11 +243,18 @@ buttonAdd.addEventListener("click", (e) => {
   addTodo();
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-  const localStorageData = localStorage.getItem("todo");
-  if (localStorageData) {
-    todos = [JSON.parse(localStorageData)];
-  }
+buttonSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchTodo();
+});
 
+buttonDeleteAll.addEventListener("click", (e) => {
+  e.preventDefault();
+  deleteAllTodo();
+});
+
+window.addEventListener("DOMContentLoaded", () => {
   inputDueDate.placeholder = new Intl.DateTimeFormat().format(new Date());
+  loadTableData();
+  console.log("Todos: ", todos);
 });
